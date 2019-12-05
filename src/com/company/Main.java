2075fbc850515c1f71648250987a80478b6f9116
VladymyrVLP <55850334+VladymyrVLP@users.noreply.github.com>
2019-12-05 11:00:26 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static int interpolPoisk(int[] znach, int element){
+        int firstindex = 0;
+        int lastIndex = znach.length-1;
+        while ((firstindex <= lastIndex) && (element>=znach[firstindex]) && (element<=znach[lastIndex])){
+
+            int res = firstindex +
+                    (((lastIndex-firstindex) /(znach[lastIndex]-znach[firstindex]))
+                            *(element-znach[firstindex]));
+
+            if (znach[res]==element){
+                return res;
+            }
+            if (znach[res]<element){
+                firstindex = res + 1;
+            }
+            else
+                lastIndex = res -1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int poisk = interpolPoisk(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},8);
+        System.out.println("Index element is " + poisk);
+
+    }
+
+}
